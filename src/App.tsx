8d49b461e8b5aebcd06b41e665f6f4c4142b48cb
Michael <michaeladejumo909@yahoo.com>
2023-01-25 18:35:40 +0100

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import { Button } from "./components/Button";
 import { Greet } from "./components/Greet";
 import { Heading } from "./components/Heading";
+import { Input } from "./components/Input";
 import { Oscar } from "./components/Oscar";
 import { Person } from "./components/Person";
 import { PersonList } from "./components/PersonList";
@@ -37,6 +39,13 @@ function App() {
       <Oscar>
         <Heading>Placeholder</Heading>
       </Oscar>
+
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button Clicked", event, id);
+        }}
+      />
+      <Input value="" handleChange={(event) => console.log(event)} />
     </div>
   );
 }
---
+++ src/components/Input.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+type InputProps = {
+  value: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export const Input = (props: InputProps) => {
+  return (
+    <input
+      placeholder="input"
+      type="text"
+      value={props.value}
+      onChange={props.handleChange}
+    />
+  );
+};
